e1c02c9b21f3f34b8ef3fae9cce7348098f4a96e
Will Graham <will.graham@ccbcc.com>
2015-10-02 16:29:33 -0400

    Added self-installer. Code is really bad and it's still too dumb to know it's already installed.

--- RunWebserverHere/RunWebserverHere/InstallUninstallForm.Designer.cs
+++ RunWebserverHere/RunWebserverHere/InstallUninstallForm.Designer.cs
@@ -32,13 +32,16 @@
             this.LogoPictureBox = new System.Windows.Forms.PictureBox();
             this.TitleLabel = new System.Windows.Forms.Label();
             this.panel1 = new System.Windows.Forms.Panel();
+            this.label1 = new System.Windows.Forms.Label();
+            this.InstallButton = new System.Windows.Forms.Button();
             ((System.ComponentModel.ISupportInitialize)(this.LogoPictureBox)).BeginInit();
+            this.panel1.SuspendLayout();
             this.SuspendLayout();
             // 
             // LogoPictureBox
             // 
             this.LogoPictureBox.Image = global::RunWebserverHere.Properties.Resources.LogoImage;
-            this.LogoPictureBox.Location = new System.Drawing.Point(12, 45);
+            this.LogoPictureBox.Location = new System.Drawing.Point(12, 142);
             this.LogoPictureBox.Name = "LogoPictureBox";
             this.LogoPictureBox.Size = new System.Drawing.Size(128, 128);
             this.LogoPictureBox.TabIndex = 0;
@@ -50,22 +53,43 @@
             this.TitleLabel.Font = new System.Drawing.Font("Microsoft Sans Serif", 21.75F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.TitleLabel.Location = new System.Drawing.Point(12, 9);
             this.TitleLabel.Name = "TitleLabel";
-            this.TitleLabel.Size = new System.Drawing.Size(288, 33);
+            this.TitleLabel.Size = new System.Drawing.Size(381, 33);
             this.TitleLabel.TabIndex = 1;
-            this.TitleLabel.Text = "RunWebserverHere";
+            this.TitleLabel.Text = "Install RunWebserverHere";
             // 
             // panel1
             // 
-            this.panel1.Location = new System.Drawing.Point(146, 45);
+            this.panel1.Controls.Add(this.label1);
+            this.panel1.Location = new System.Drawing.Point(12, 45);
             this.panel1.Name = "panel1";
-            this.panel1.Size = new System.Drawing.Size(306, 225);
+            this.panel1.Size = new System.Drawing.Size(440, 91);
             this.panel1.TabIndex = 2;
+            // 
+            // label1
+            // 
+            this.label1.Location = new System.Drawing.Point(3, 0);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(434, 91);
+            this.label1.TabIndex = 0;
+            this.label1.Text = resources.GetString("label1.Text");
+            // 
+            // InstallButton
+            // 
+            this.InstallButton.Location = new System.Drawing.Point(334, 239);
+            this.InstallButton.Name = "InstallButton";
+            this.InstallButton.Size = new System.Drawing.Size(115, 31);
+            this.InstallButton.TabIndex = 3;
+            this.InstallButton.Text = "Install!";
+            this.InstallButton.UseVisualStyleBackColor = true;
+            this.InstallButton.Click += new System.EventHandler(this.InstallButton_Click);
             // 
             // InstallUninstallForm
             // 
+            this.AcceptButton = this.InstallButton;
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(464, 282);
+            this.Controls.Add(this.InstallButton);
             this.Controls.Add(this.panel1);
             this.Controls.Add(this.TitleLabel);
             this.Controls.Add(this.LogoPictureBox);
@@ -76,6 +100,7 @@
             this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
             this.Text = "RunWebserverHere";
             ((System.ComponentModel.ISupportInitialize)(this.LogoPictureBox)).EndInit();
+            this.panel1.ResumeLayout(false);
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -86,5 +111,7 @@
         private System.Windows.Forms.PictureBox LogoPictureBox;
         private System.Windows.Forms.Label TitleLabel;
         private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button InstallButton;
     }
 }
--- RunWebserverHere/RunWebserverHere/InstallUninstallForm.cs
+++ RunWebserverHere/RunWebserverHere/InstallUninstallForm.cs
@@ -4,6 +4,7 @@ using System.ComponentModel;
 using System.Data;
 using System.Drawing;
 using System.Linq;
+using System.Reflection;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
@@ -15,6 +16,33 @@ namespace RunWebserverHere
         public InstallUninstallForm()
         {
             InitializeComponent();
+        }
+
+        private void InstallButton_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                var key = Microsoft.Win32.Registry.ClassesRoot.CreateSubKey("Directory\\shell\\runwebserverhere", Microsoft.Win32.RegistryKeyPermissionCheck.ReadWriteSubTree);
+                key.SetValue(null, "Run Webserver Here");
+                var commandKey = key.CreateSubKey("command");
+                commandKey.SetValue(null, string.Format("\"{0}\" \"%1\"", Assembly.GetExecutingAssembly().Location));
+
+                var key2 = Microsoft.Win32.Registry.ClassesRoot.CreateSubKey("Directory\\shell\\runwebserverhere_cfg", Microsoft.Win32.RegistryKeyPermissionCheck.ReadWriteSubTree);
+                key2.SetValue(null, "Configure Webserver Here");
+                key2.SetValue("Extended", "");
+                var commandKey2 = key2.CreateSubKey("command");
+                commandKey2.SetValue(null, string.Format("\"{0}\" \"%1\" --configure", Assembly.GetExecutingAssembly().Location));
+
+                MessageBox.Show("Installed successfully!", "RunWebserverHere", MessageBoxButtons.OK, MessageBoxIcon.Information);
+                this.DialogResult = System.Windows.Forms.DialogResult.OK;
+                this.Close();
+            }
+            catch
+            {
+                MessageBox.Show("Install failed! Try running as admin.", "RunWebserverHere", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                this.DialogResult = System.Windows.Forms.DialogResult.Cancel;
+                this.Close();
+            }
         }
     }
 }
